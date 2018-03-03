597a208236440eac3b446f534b8bf1b29ac4b7a7
michaelwestbrook <michaelrwestbrook@gmail.com>
2018-03-03 03:51:48 -0800

    Holy shit I was using the original deploy script this whole time?!?!

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1,15 +1,6 @@
-$FileTypes = @('lua','txt','xml')
-$Files = @()
 $ModName = "CrownPointerThing"
 $ModDir = "$env:USERPROFILE\Documents\Elder Scrolls Online\live\AddOns\$ModName"
-Remove-Item -Path $ModDir -Recurse
-New-Item -Path $ModDir -itemtype directory -force
-foreach($FileType in $FileTypes) {
-  $FileName = "./$ModName.$FileType"
-  Copy-Item "$FileName" -Destination "$ModDir"
-  Write-Host "Copied $FileName to $ModDir"
-}
-foreach($File in $Files) {
-  Copy-Item $File -Destination "$ModDir"
-  Write-Host "Copied $File to $ModDir"
-}
+Remove-Item -Path "$ModDir" -Recurse
+New-Item -Path "$ModDir" -itemtype directory -force
+Copy-Item -Path "$pwd\*" -Exclude @("deploy", ".git") -Force -Recurse -Destination "$ModDir"
+Write-Host "Copied $pwd\* to $ModDir"
